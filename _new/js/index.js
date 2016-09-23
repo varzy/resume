1e5b3b1cb583258c25ad85b5fb62f2ae36cb9408
zy <s@zyis.me>
2016-09-23 10:05:28 +0800

    # update

---
+++ _new/js/index.js
@@ -0,0 +1,31 @@
+// #1
+
+// $(function() {
+
+//   $(window).resize(function() {
+//     var width = $(this).width();
+
+//     if (width < 991) {
+//       $(".avater-1").show();
+//       $(".avater-2").hide();
+//     } else {
+//       $(".avater-2").show();
+//       $(".avater-1").hide();
+//     }
+
+//   })
+
+// })
+
+
+
+// #2
+
+// $(function() {
+//   if ($(window).width() < 991) {
+//     $(".avater-1").show();
+//     $(".avater-2").hide();
+//   };
+
+// });
+
