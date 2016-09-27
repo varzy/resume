4d8a687aecf9b9ef251c9d24b67e2720f119e308
zy <s@zyis.me>
2016-09-27 13:13:48 +0800

    # fix - 调整 js 窗口断点为 767

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(function() {
   // $("*").fadeIn;
 
   // wechat qr code
-  if (window.innerWidth > 829) {
+  if (window.innerWidth > 767) {
     $(".wechatqr-link").click(function() {
       $("#wechatqr").fadeToggle();
       $("#bg-grey").fadeToggle();
