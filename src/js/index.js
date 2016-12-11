9c5c267d2ebd6c43da3cd1f83b313e21e95169e8
zy <s@zyis.me>
2016-12-11 18:45:04 +0800

    f51: test end. reback

---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+$(function() {
+
+  if (window.innerWidth > 767) {
+    $(".wechatqr-link").click(function() {
+      $("#wechatqr").fadeToggle();
+      $("#bg-grey").fadeToggle();
+      $("#btn-close").fadeToggle();
+    })
+
+    $("#btn-close").click(function() {
+      $("#wechatqr").fadeToggle();
+      $("#bg-grey").fadeToggle();
+      $("#btn-close").fadeToggle();
+    })
+
+  }
+
+
+
+
+
+
+
+
+
+});
