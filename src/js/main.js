d3b1d9a7122e93f1a560323ba7a55c142fa3e296
zy <s@zyis.me>
2017-01-06 14:42:25 +0800

    u1: add js

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,18 @@
 window.onload = function () {
 
+  var aSkillSubModule = document.getElementsByClassName("skill")[0].getElementsByClassName("sub-module");
 
+  for (var i = 0; i < aSkillSubModule.length; i++) {
+    aSkillSubModule[i].index = i;
+    aSkillSubModule[i].onclick = function () {
+      switch (this.index) {
+        case 0:
+
+          break;
+      }
+    }
+  }
+
+
+  // ! end window.onload()
 }
