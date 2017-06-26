adadece19ce2b181aa4bcf642aa46ce0c2f331af
zy <s@zyis.me>
2017-06-26 20:08:51 +0800

    complete page's structure
    - update styles
    - complete html
    - temporarily remove my own js

--- src/js/_entry.js
+++ src/js/_entry.js
@@ -1,7 +1,7 @@
 /**
  * scss && css part
  */
-require("normalize.css");
+// require("normalize.css");
 // wingcss's npm module has not `.card` classes, so include it by self
 require("../lib/wing.css");
 require("../scss/preset.scss");
--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,34 @@
-// include libs
+/**
+ * include libs
+ * @type {jQuery}
+ */
 const $ = require("jquery");
 
-// main part
-$(function () {
-  let cnLang = require("../lang/cn.json");
-  let enLang = require("../lang/en.json");
-  loadLang(cnLang);
+/**
+ * main part
+ */
 
-  $("#to_cn").click(function () {
-    loadLang(cnLang);
-  });
+// toggle language
 
-  $("#to_en").click(function () {
-    loadLang(enLang);
-  });
-
-  function loadLang(langData) {
-    $(langData.header).each(function (index, value) {
-      $("#name").html(value.name);
-      $("#intro").html(value.intro);
-    });
-  }
-
-
-});
+// $(function () {
+//   let cnLang = require("../lang/cn.json");
+//   let enLang = require("../lang/en.json");
+//   loadLang(cnLang);
+//
+//   $("#to_cn").click(function () {
+//     loadLang(cnLang);
+//   });
+//
+//   $("#to_en").click(function () {
+//     loadLang(enLang);
+//   });
+//
+//   function loadLang(langData) {
+//     $(langData.header).each(function (index, value) {
+//       $("#name").html(value.name);
+//       $("#intro").html(value.intro);
+//     });
+//   }
+//
+//
+// });
