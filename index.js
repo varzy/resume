7a29db2ff5864e36bd5942d0b5d292389af2021f
zy <varzy@live.com>
2020-03-28 14:52:22 +0800

    格式化代码

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-window.onload = function() {
+window.onload = function () {
   setFavicon();
-  registerPdfDownloader();
-  registerDocxDownloader();
-}
+};
 
 function setFavicon() {
-  var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-  link.type = 'image/x-icon';
-  link.rel = 'shortcut icon';
-  link.href = './favicon.png';
-  document.getElementsByTagName('head')[0].appendChild(link);
-}
-
-function registerPdfDownloader() {
-  document.
+  var link =
+    document.querySelector("link[rel*='icon']") ||
+    document.createElement("link");
+  link.type = "image/x-icon";
+  link.rel = "shortcut icon";
+  link.href = "./favicon.png";
+  document.getElementsByTagName("head")[0].appendChild(link);
 }
