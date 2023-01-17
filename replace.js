706a7018f8ad2b1223306b3a1ebce262e85f4c10
zy <varzy@live.com>
2023-01-18 06:10:27 +0800

    Update Resume.

--- replace.js
+++ replace.js
@@ -2,7 +2,7 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 
 // 加载由 Typora 导出的 html
-const sourceHtml = fs.readFileSync('./exports/resume.html');
+const sourceHtml = fs.readFileSync('./files/resume.html');
 const $ = cheerio.load(sourceHtml);
 const $head = $($('head')[0]);
 const $body = $('body');
@@ -17,7 +17,7 @@ $head.append(`<link ref="icon" type="image/x-icon" href="./favicon.png">`);
 $head.append(`<link rel="stylesheet" href="./index.css">`)
 
 // 插入下载按钮
-$body.append(`<a class="pin pdf_downloader" href="./exports/resume.pdf" download="赵越-前端-15565252838.pdf">PDF</a>`);
-$body.append(`<a class="pin md_downloader" href="./exports/resume.md" download="赵越-前端-15565252838.md">MD</a>`);
+$body.append(`<a class="pin pdf_downloader" href="./files/resume.pdf" download="赵越-前端-15565252838.pdf">PDF</a>`);
+$body.append(`<a class="pin md_downloader" href="./files/resume.md" download="赵越-前端-15565252838.md">MD</a>`);
 
 fs.writeFileSync('index.html', $.root().html());
